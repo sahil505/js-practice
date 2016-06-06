1862c6ab7cac582c2433bc01be5bd84bac240bec
Allie Ivener <adi2hf@virginia.edu>
2016-06-06 15:07:57 +0200

    tests finished

---
+++ day6.js
@@ -0,0 +1,55 @@
+function average(numbers_list) {
+
+}
+
+function assignGrade(grade) {
+
+}
+
+function pluralize(num, noun) {
+
+}
+
+function longestWord(sentence) {
+
+}
+
+function palindrome(word) {
+
+}
+
+function letterCounter(phrase, letter) {
+
+}
+
+function longestPalindrome(sentence) {
+
+}
+
+function doItTwice(f) {
+
+}
+
+function objectFun(first, last, age, email, color) {
+	
+}
+
+function numChildren(obj) {
+	
+}
+
+function greeting(name) {
+	
+}
+
+function sayHello(first, last, greet) {
+
+}
+
+function goodnight(name) {
+
+}
+
+function sayGoodnight(name, status, message) {
+
+}
